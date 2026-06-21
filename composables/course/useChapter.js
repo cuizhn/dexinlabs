f4b42895c1a48984381fa8e212293267a9abf235
cuizhn <cuigaozheng@foxmail.com>
2026-06-22 07:31:21 +0800

    更新 useChapter.js

--- composables/course/useChapter.js
+++ composables/course/useChapter.js
@@ -1,67 +1,17 @@
-// composables/useChapter.js
-
 import { chapterRepository } from '~/repositories/chapterRepository'
 
 export function useChapter() {
 
-  /**
-   * 获取章节内容
-   */
   async function getChapter(courseSlug, chapterSlug) {
-
-    const chapter =
-      await chapterRepository.findBySlug(
-        courseSlug,
-        chapterSlug
-      )
-
-    return chapter
+    return await chapterRepository.findBySlug(courseSlug, chapterSlug)
   }
 
-  /**
-   * 获取章节导航
-   */
-  async function getNavigation(courseSlug, chapterSlug) {
-
-    const navigation =
-      await chapterRepository.getNavigation(
-        courseSlug,
-        chapterSlug
-      )
-
-    return navigation
-  }
-
-  /**
-   * 页面级组合：章节 + 导航
-   */
-  async function loadChapter(courseSlug, chapterSlug) {
-
-    const chapter =
-      await getChapter(courseSlug, chapterSlug)
-
-    if (!chapter) {
-      return {
-        chapter: null,
-        navigation: {
-          prev: null,
-          next: null
-        }
-      }
-    }
-
-    const navigation =
-      await getNavigation(courseSlug, chapterSlug)
-
-    return {
-      chapter,
-      navigation
-    }
+  async function getChapters(courseSlug) {
+    return await chapterRepository.findAllByCourse(courseSlug)
   }
 
   return {
     getChapter,
-    getNavigation,
-    loadChapter
+    getChapters
   }
 }
