f73e31e713026e1c6c466d3d7041621983ae2a8a
cuizhn <cuigaozheng@foxmail.com>
2026-06-21 16:37:33 +0800

    更新 useChapter.js

--- composables/course/useChapter.js
+++ composables/course/useChapter.js
@@ -1,35 +1,67 @@
-import { chapterRepository }
-  from '~/repositories/chapterRepository'
+// composables/useChapter.js
 
-export async function useChapter(
-  courseSlug,
-  chapterSlug
-) {
+import { chapterRepository } from '~/repositories/chapterRepository'
 
-  const chapter =
-    await chapterRepository.findBySlug(
-      courseSlug,
-      chapterSlug
-    )
+export function useChapter() {
 
-  if (!chapter) {
-    return {
-      chapter: null,
-      navigation: {
-        prev: null,
-        next: null
+  /**
+   * 获取章节内容
+   */
+  async function getChapter(courseSlug, chapterSlug) {
+
+    const chapter =
+      await chapterRepository.findBySlug(
+        courseSlug,
+        chapterSlug
+      )
+
+    return chapter
+  }
+
+  /**
+   * 获取章节导航
+   */
+  async function getNavigation(courseSlug, chapterSlug) {
+
+    const navigation =
+      await chapterRepository.getNavigation(
+        courseSlug,
+        chapterSlug
+      )
+
+    return navigation
+  }
+
+  /**
+   * 页面级组合：章节 + 导航
+   */
+  async function loadChapter(courseSlug, chapterSlug) {
+
+    const chapter =
+      await getChapter(courseSlug, chapterSlug)
+
+    if (!chapter) {
+      return {
+        chapter: null,
+        navigation: {
+          prev: null,
+          next: null
+        }
       }
+    }
+
+    const navigation =
+      await getNavigation(courseSlug, chapterSlug)
+
+    return {
+      chapter,
+      navigation
     }
   }
 
-  const navigation =
-    await chapterRepository.getNavigation(
-      courseSlug,
-      chapterSlug
-    )
-
   return {
-    chapter,
-    navigation
+    getChapter,
+    getNavigation,
+    loadChapter
   }
 }
