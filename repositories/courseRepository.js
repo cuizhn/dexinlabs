a4199b72a45e1d5de6f47276be8a7f893af509b0
cuizhn <cuigaozheng@foxmail.com>
2026-06-21 22:41:13 +0800

    更新 courseRepository.js

--- repositories/courseRepository.js
+++ repositories/courseRepository.js
@@ -2,7 +2,7 @@ export const courseRepository = {
 async findAll() {
 
         return await queryCollection('courses')
-      .order('order', 'ASC')
+      
       .all()
   },
   async findBySlug(slug) {
