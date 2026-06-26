175c90648816f6bd874ea57ff210e5bde7c32b5b
cuizhn <cuigaozheng@foxmail.com>
2026-06-26 20:04:28 +0800

    删除 [slug].get.js

--- server/api/lessons/[slug].get.js
+++
@@ -1,7 +0,0 @@
-export default defineEventHandler(async (event) => {
-  const slug = getRouterParam(event, "slug");
-
-  return await queryCollection(event, "lesson")
-    .where("slug", "=", slug)
-    .first();
-});
