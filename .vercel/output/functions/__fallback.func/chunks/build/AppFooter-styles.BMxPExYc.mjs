b7a104966d2b6532c575db8f223eaf453e590032
cuizhn <cuigaozheng@foxmail.com>
2026-07-08 06:34:29 +0800

    1

--- .vercel/output/functions/__fallback.func/chunks/build/AppFooter-styles.BMxPExYc.mjs
+++
@@ -1,8 +0,0 @@
-import { A as AppFooter_vue_vue_type_style_index_0_scoped_4febeefd_lang } from './default-styles-2.mjs-B8URDm-n.mjs';
-
-const AppFooterStyles_BMxPExYc = [
-  AppFooter_vue_vue_type_style_index_0_scoped_4febeefd_lang
-];
-
-export { AppFooterStyles_BMxPExYc as default };
-//# sourceMappingURL=AppFooter-styles.BMxPExYc.mjs.map
